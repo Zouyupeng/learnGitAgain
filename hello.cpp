0f5aad46c5e25e46038510ed8b004dcd07b45424
Oceanknight <oceanknit@foxmail.com>
2021-02-05 12:29:16 +0800

    hello world again

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: Oceanknight
+	> Mail: 2827280689@qq.com
+	> Created Time: 2021年02月05日 星期五 12时27分16秒
+ ************************************************************************/
+
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "hello,World!" << endl;
+    return 0;
+}
